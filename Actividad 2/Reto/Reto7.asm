c5e1a02ea4fcf31b83012737aa89acbaa7096759
CarlosManrique06 <cajumalo06@gmail.com>
2025-08-04 21:13:12 -0500

    Terminando los retos

---
+++ Actividad 2/Reto/Reto7.asm
@@ -0,0 +1,21 @@
+@10
+D=A  
+@var
+M=D // se guarda 10 en var
+
+@5
+D=A
+@bis  // se guarda 5 en var
+M=D
+
+@var
+D=A
+@PUNT 
+M=D   // se guarda la direccion de var en PUNT
+
+@PUNT  
+A=M
+D=M
+@bis
+M=D    // se guarda el valor de la direccion al que apunta el puntero y lo guarda en bis, 
+       // osea apunta a la direccion var, toma ese valor que tiene guardado var en este caso 10 y lo guarda en bis
